edfcd427e1a9f7f5a403d2a1b5db4d3af869ec20
elene shetsiruli <ellebuto@gmail.com>
2024-04-14 03:32:39 +0400

    "com"

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,49 +1,21 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2021: true,
-  },
+  env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
-    "plugin:styled-components/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  plugins: ["react-hooks", "styled-components"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
     "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
   },
 };
-
-// module.exports = {
-//   root: true,
-//   env: { browser: true, es2020: true },
-//   extends: [
-//     'eslint:recommended',
-//     'plugin:react/recommended',
-//     'plugin:react/jsx-runtime',
-//     'plugin:react-hooks/recommended',
-//   ],
-//   ignorePatterns: ['dist', '.eslintrc.cjs'],
-//   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-//   settings: { react: { version: '18.2' } },
-//   plugins: ['react-refresh'],
-//   rules: {
-//     'react/jsx-no-target-blank': 'off',
-//     'react-refresh/only-export-components': [
-//       'warn',
-//       { allowConstantExport: true },
-//     ],
-//   },
-// }
